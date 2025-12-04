b64beaa2b792bd4702ae10fdba88da4956f85837
muh-hsuan <muhhsuan@gmail.com>
2025-12-04 18:09:49 +0800

    v1.0.1

--- app.config.ts
+++ app.config.ts
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         ...config,
         name: getAppName(),
         slug: "snake",
-        version: "1.0.0",
+        version: "1.0.1",
         orientation: "portrait",
         icon: "./assets/images/icon.png",
         scheme: "snakeapp",
